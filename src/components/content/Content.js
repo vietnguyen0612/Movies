d8f36619463a69aa7ab92e11866c240db7d54b79
Viet Nguyen <vanvietn73@gmail.com>
2022-10-14 01:26:37 +0700

    slider movie 1

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,35 +1,49 @@
+import './content.scss';
+
+import {AiOutlineLeft} from 'react-icons/ai'
+
+
+
+
+
 const movies = [
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
-    'https://cdn-www.vinid.net/cb56ecf5-phim-marvel-1.jpg',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
+    'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2021/12/poster-chinh-thuc-cua-shang-chi.jpg?fit=700%2C20000&quality=95&ssl=1',
     
-]
+];
 
 
 function Content() {
+    
     return (  
         <div className="content">
             <h1>Movies</h1>
-            <div>
+            <div className="Movies-content">
                 {
                     movies.map((e,index) => (
-                        <div key={index}>
-                            <img src={e} alt="" />
-                            <div>movies name</div>
+                        <div key={index} className='movie'>
+                            <img src={e} alt="" className="movie-img"/>
+                            <div className="movie-name">movies name</div>
                         </div>
                     ))
                 }
             </div>
+            
+            <AiOutlineLeft className='arrow left'/>
+            <AiOutlineLeft className='arrow right'/>
+
+            
         </div>
     );
 }
