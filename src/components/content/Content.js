d86b59706c3a2b419c2ec0a8eb78f5dbb04cc82d
Viet Nguyen <vanvietn73@gmail.com>
2022-10-14 23:27:03 +0700

    scroll slider

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,6 +1,7 @@
 import './content.scss';
 
 import {AiOutlineLeft} from 'react-icons/ai'
+import { useEffect, useRef, useState } from 'react';
 
 
 
@@ -25,23 +26,90 @@ const movies = [
 
 
 function Content() {
+    const sliderRef = useRef()
+    const movieRef = useRef()
+
+    const [isDrag, setIsDrag] = useState(false)
+    const [dragDown, setDragDown] = useState(0)    
+    const [dragMovie, setDragMovie] = useState(0)
+
     
+    const handleClickSlider = (wap)=> {
+        var screenSize = sliderRef.current.clientWidth
+        var constWidth = sliderRef.current.scrollWidth
+
+
+        if(wap === 'left') {
+            if(constWidth - (constWidth - sliderRef.current.scrollLeft) > movieRef.current.scrollWidth*2 ) {
+                sliderRef.current.scrollLeft -= movieRef.current.scrollWidth*2
+            }else {
+                sliderRef.current.scrollLeft = 0
+            }
+        }
+        else {
+            if(constWidth - (sliderRef.current.scrollLeft + screenSize) > movieRef.current.scrollWidth*2) {
+                sliderRef.current.scrollLeft += movieRef.current.scrollWidth*2
+            }else {
+                sliderRef.current.scrollLeft = constWidth - screenSize
+            }
+        }
+    }
+
+    useEffect(() => {
+        if(isDrag) {
+            if(dragDown > dragMovie) {
+                // console.log('sang phải')
+                // handleClickSlider('')
+                sliderRef.current.scrollLeft +=(dragDown - dragMovie)
+                
+            }
+            if(dragDown < dragMovie) {
+                // handleClickSlider('left')
+                // console.log('sang trái')
+                sliderRef.current.scrollLeft -= (dragMovie - dragDown)
+
+            }
+        }
+    },[isDrag,dragDown,dragMovie])
+
+    
+    const handleOnDragStart = (e) => {
+        setIsDrag(true);
+        setDragDown(e.screenX)
+        
+    }
+    const handleOnDragEnd = (e) => {
+        setIsDrag(false)
+        
+    }
+    const handleOnDragEnter = (e) => {
+        setDragMovie(e.screenX)
+        
+    }
+
     return (  
-        <div className="content">
+        <div className="content" draggable={false}>
             <h1>Movies</h1>
-            <div className="Movies-content">
+            <div 
+                className="Movies-content" 
+                ref={sliderRef} 
+                draggable={true}
+                onDragEnter={handleOnDragEnter}
+                onDragStart= {handleOnDragStart}
+                onDragEnd= {handleOnDragEnd}
+            >
                 {
                     movies.map((e,index) => (
-                        <div key={index} className='movie'>
-                            <img src={e} alt="" className="movie-img"/>
+                        <div key={index} className='movie' ref={movieRef} draggable={false}>
+                            <img src={e} alt="" className="movie-img" draggable={false}/>
                             <div className="movie-name">movies name</div>
                         </div>
                     ))
                 }
             </div>
             
-            <AiOutlineLeft className='arrow left'/>
-            <AiOutlineLeft className='arrow right'/>
+            <AiOutlineLeft className='arrow left' onClick={()=> handleClickSlider('left')}/>
+            <AiOutlineLeft className='arrow right' onClick={()=> handleClickSlider('')}/>
 
             
         </div>
